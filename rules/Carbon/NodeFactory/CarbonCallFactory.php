66f75721796cae859b28b19196c4484f23274026
Florisbosch <floris-bosch@live.nl>
2024-07-25 11:20:46 +0200

    Rewrite Carbon Call Factory to combine regex and fallback to parse

--- rules/Carbon/NodeFactory/CarbonCallFactory.php
+++ rules/Carbon/NodeFactory/CarbonCallFactory.php
@@ -1,10 +1,9 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace Rector\Carbon\NodeFactory;
 
-use Nette\Utils\Strings;
+use RectorPrefix202407\Nette\Utils\Strings;
 use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\StaticCall;
@@ -12,152 +11,49 @@ use PhpParser\Node\Identifier;
 use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\Scalar\LNumber;
 use PhpParser\Node\Scalar\String_;
-
 final class CarbonCallFactory
 {
     /**
      * @var string
-     * @see https://regex101.com/r/9vGt8r/1
+     * @see https://regex101.com/r/19qPHr/1
      */
-    private const PLUS_DAY_COUNT_REGEX = '#\+(\s+)?(?<count>\d+)(\s+)?(day|days)#';
+    private const PLUS_MINUS_COUNT_REGEX = '#(?<operator>\+|-)(\\s+)?(?<count>\\d+)(\s+)?(?<unit>sec|second|seconds|min|minute|minutes|hour|hours|day|days|week|weeks|month|months)#';
 
     /**
-     * @var string
-     * @see https://regex101.com/r/pqOPg6/1
+     * @return \PhpParser\Node\Expr\MethodCall|\PhpParser\Node\Expr\StaticCall
      */
-    private const MINUS_DAY_COUNT_REGEX = '#-(\s+)?(?<count>\d+)(\s+)?(day|days)#';
-
-    /**
-     * @var string
-     * @see https://regex101.com/r/6VUUQF/1
-     */
-    private const PLUS_MONTH_COUNT_REGEX = '#\+(\s+)?(?<count>\d+)(\s+)?(month|months)#';
-
-    /**
-     * @var string
-     * @see https://regex101.com/r/dWRjk5/1
-     */
-    private const PLUS_HOUR_COUNT_REGEX = '#\+(\s+)?(?<count>\d+)(\s+)?(hour|hours)#';
-
-    /**
-     * @var string
-     * @see https://regex101.com/r/dfK0Ri/1
-     */
-    private const PLUS_MINUTE_COUNT_REGEX = '#\+(\s+)?(?<count>\d+)(\s+)?(minute|minuts)#';
-
-    /**
-     * @var string
-     * @see https://regex101.com/r/o7QDYL/1
-     */
-    private const PLUS_SECOND_COUNT_REGEX = '#\+(\s+)?(?<count>\d+)(\s+)?(second|seconds)#';
-
-    /**
-     * @var string
-     * @see https://regex101.com/r/dWRjk5/1
-     */
-    private const PLUS_HOUR_COUNT_REGEX = '#\+(\s+)?(?<count>\d+)(\s+)?(hour|hours)#';
-
-    /**
-     * @var string
-     * @see https://regex101.com/r/dfK0Ri/1
-     */
-    private const PLUS_MINUTE_COUNT_REGEX = '#\+(\s+)?(?<count>\d+)(\s+)?(minute|minuts)#';
-
-    /**
-     * @var string
-     * @see https://regex101.com/r/o7QDYL/1
-     */
-    private const PLUS_SECOND_COUNT_REGEX = '#\+(\s+)?(?<count>\d+)(\s+)?(second|seconds)#';
-
-    /**
-     * @var string
-     * @see https://regex101.com/r/IvyT7w/1
-     */
-    private const MINUS_MONTH_COUNT_REGEX = '#-(\s+)?(?<count>\d+)(\s+)?(month|months)#';
-
-    /**
-     * @var string
-     * @see https://regex101.com/r/bICKg6/1
-     */
-    private const MINUS_HOUR_COUNT_REGEX = '#-(\s+)?(?<count>\d+)(\s+)?(hour|hours)#';
-
-    /**
-     * @var string
-     * @see https://regex101.com/r/WILFvX/1
-     */
-    private const MINUS_MINUTE_COUNT_REGEX = '#-(\s+)?(?<count>\d+)(\s+)?(minute|minutes)#';
-
-    /**
-     * @var string
-     * @see https://regex101.com/r/FwCUup/1
-     */
-    private const MINUS_SECOND_COUNT_REGEX = '#-(\s+)?(?<count>\d+)(\s+)?(second|seconds)#';
-
-    /**
-     * @var string
-     * @see https://regex101.com/r/bICKg6/1
-     */
-    private const MINUS_HOUR_COUNT_REGEX = '#-(\s+)?(?<count>\d+)(\s+)?(hour|hours)#';
-
-    /**
-     * @var string
-     * @see https://regex101.com/r/WILFvX/1
-     */
-    private const MINUS_MINUTE_COUNT_REGEX = '#-(\s+)?(?<count>\d+)(\s+)?(minute|minutes)#';
-
-    /**
-     * @var string
-     * @see https://regex101.com/r/FwCUup/1
-     */
-    private const MINUS_SECOND_COUNT_REGEX = '#-(\s+)?(?<count>\d+)(\s+)?(second|seconds)#';
-
-    /**
-     * @var array<self::*_REGEX, string>
-     */
-    private const REGEX_TO_METHOD_NAME_MAP = [
-        self::PLUS_DAY_COUNT_REGEX => 'addDays',
-        self::MINUS_DAY_COUNT_REGEX => 'subDays',
-        self::PLUS_MONTH_COUNT_REGEX => 'addMonths',
-        self::MINUS_MONTH_COUNT_REGEX => 'subMonths',
-        self::PLUS_HOUR_COUNT_REGEX => 'addHours',
-        self::MINUS_HOUR_COUNT_REGEX => 'subHours',
-        self::PLUS_MINUTE_COUNT_REGEX => 'addMinutes',
-        self::MINUS_MINUTE_COUNT_REGEX => 'subMinutes',
-        self::PLUS_SECOND_COUNT_REGEX => 'addSeconds',
-        self::MINUS_SECOND_COUNT_REGEX => 'subSeconds',
-    ];
-
-    public function createFromDateTimeString(
-        FullyQualified $carbonFullyQualified,
-        String_ $string
-    ): MethodCall|StaticCall {
+    public function createFromDateTimeString(FullyQualified $carbonFullyQualified, String_ $string)
+    {
         $dateTimeValue = $string->value;
-        if ($dateTimeValue === 'now') {
-            return new StaticCall($carbonFullyQualified, new Identifier('now'));
+        if (in_array($dateTimeValue, ['now', 'today', 'yesterday', 'tomorrow'])) {
+            return new StaticCall($carbonFullyQualified, new Identifier($dateTimeValue));
         }
 
-        if ($dateTimeValue === 'today') {
-            return new StaticCall($carbonFullyQualified, new Identifier('today'));
+        $startDate = Strings::match($dateTimeValue, '#now|yesterday|today|tomorrow#')[0] ?? 'now';
+        $carbonCall = new StaticCall($carbonFullyQualified, new Identifier($startDate));
+
+        $match = Strings::match($dateTimeValue, self::PLUS_MINUS_COUNT_REGEX);
+
+        if ($match === null) {
+            return new StaticCall($carbonFullyQualified, new Identifier('parse'), [new Arg($string)]);
         }
 
-        $hasToday = Strings::match($dateTimeValue, '#today#');
-        if ($hasToday !== null) {
-            $carbonCall = new StaticCall($carbonFullyQualified, new Identifier('today'));
-        } else {
-            $carbonCall = new StaticCall($carbonFullyQualified, new Identifier('now'));
-        }
+        $countLNumber = new LNumber((int) $match['count']);
 
-        foreach (self::REGEX_TO_METHOD_NAME_MAP as $regex => $methodName) {
-            $match = Strings::match($dateTimeValue, $regex);
-            if ($match === null) {
-                continue;
-            }
+        $unit = match((string) $match['unit']) {
+            'sec', 'second', 'seconds' => 'seconds',
+            'min', 'minute', 'minutes' => 'minutes',
+            'hour', 'hours' => 'hours',
+            'day', 'days' => 'days',
+            'week', 'weeks' => 'weeks',
+            'month', 'months' => 'months',
+        };
 
-            $countLNumber = new LNumber((int) $match['count']);
+        $methodName = match((string) $match['operator']) {
+            '+' => 'add',
+            '-' => 'sub',
+        } . ucfirst($unit);
 
-            $carbonCall = new MethodCall($carbonCall, new Identifier($methodName), [new Arg($countLNumber)]);
-        }
-
-        return $carbonCall;
+        return new MethodCall($carbonCall, new Identifier($methodName), [new Arg($countLNumber)]);
     }
 }
